f689a23611ae909f8aa263d3f26f2d1ca6b41895
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-02-19 19:14:41 +0100

    move api to dedicated file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const AWS = require('aws-sdk');
 const uuid = require('uuid/v4')
+const apiRouter = require('./server/api')
 
 const MARKERS_TABLE = process.env.MARKERS_TABLE;
 const IS_OFFLINE = process.env.IS_OFFLINE;
@@ -20,75 +21,7 @@ if (IS_OFFLINE === 'true') {
 }
 
 app.use(bodyParser.json({ strict: false }));
-
-app.get('/api/markers', function (req, res) {
-  const params = {
-    TableName: MARKERS_TABLE
-  }
-
-  dynamoDb.scan(params, (error, result) => {
-    if (error) {
-      console.log(error);
-      return res.status(400).json({ error: 'Could not get markers' });
-    }
-
-    if (result && result.Items) {
-      return res.json({ items: result.Items, meta: {
-        total: result.Count
-      } });
-    }
-
-    console.log(error);
-    return res.status(400).json({ error: 'Could not get markers' });
-  });
-})
-
-app.get('/api/markers/:markerId', function (req, res) {
-  if (!req.params.markerId) {
-    return res.status(400).json({error: 'Marker ID not set'})
-  }
-
-  const params = {
-    TableName: MARKERS_TABLE,
-    Key: {
-      id: req.params.markerId,
-    },
-  }
-
-  dynamoDb.get(params, (error, result) => {
-    if (error) {
-      console.log(error);
-      return res.status(400).json({ error: 'Could not get marker' });
-    }
-    if (result && result.Item) {
-      const {content, id} = result.Item;
-      return res.json({ content, id });
-    } else {
-      return res.status(404).json({ error: "Marker not found" });
-    }
-  });
-})
-
-app.post('/api/markers', function (req, res) {
-  const { lattitude, longitude } = req.body;
-  if (!lattitude || !longitude) {
-    res.status(400).json({ error: 'Lat or Long not set' });
-  }
-
-  const id = uuid()
-  const params = {
-    TableName: MARKERS_TABLE,
-    Item: {id,lattitude, longitude},
-  };
-
-  dynamoDb.put(params, (error) => {
-    if (error) {
-      console.log(error);
-      res.status(400).json({ error: 'Could not create marker' });
-    }
-    res.json({ id, lattitude, longitude });
-  });
-})
+app.use('/api', apiRouter(dynamoDb))
 
 
 module.exports.handler = serverless(app);
---
+++ server/api.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const router = express.Router();
+const uuid = require('uuid/v4')
+
+const MARKERS_TABLE = process.env.MARKERS_TABLE;
+
+const getRouter = (db) => {
+  router.get('/markers', function (req, res) {
+    const params = {
+      TableName: MARKERS_TABLE
+    }
+
+    db.scan(params, (error, result) => {
+      if (error) {
+        console.log(error);
+        return res.status(400).json({ error: 'Could not get markers' });
+      }
+
+      if (result && result.Items) {
+        return res.json({ items: result.Items, meta: {
+          total: result.Count
+        } });
+      }
+
+      console.log(error);
+      return res.status(400).json({ error: 'Could not get markers' });
+    });
+  })
+
+  router.get('/markers/:markerId', function (req, res) {
+    if (!req.params.markerId) {
+      return res.status(400).json({error: 'Marker ID not set'})
+    }
+
+    const params = {
+      TableName: MARKERS_TABLE,
+      Key: {
+        id: req.params.markerId,
+      },
+    }
+
+    db.get(params, (error, result) => {
+      if (error) {
+        console.log(error);
+        return res.status(400).json({ error: 'Could not get marker' });
+      }
+      if (result && result.Item) {
+        const {content, id} = result.Item;
+        return res.json({ content, id });
+      } else {
+        return res.status(404).json({ error: "Marker not found" });
+      }
+    });
+  })
+
+  router.post('/markers', function (req, res) {
+    const { lattitude, longitude } = req.body;
+    if (!lattitude || !longitude) {
+      res.status(400).json({ error: 'Lat or Long not set' });
+    }
+
+    const id = uuid()
+    const params = {
+      TableName: MARKERS_TABLE,
+      Item: {id,lattitude, longitude},
+    };
+
+    db.put(params, (error) => {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ error: 'Could not create marker' });
+      }
+      res.json({ id, lattitude, longitude });
+    });
+  })
+
+  return router
+}
+
+module.exports = getRouter
