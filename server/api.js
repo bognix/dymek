4b654c76cb3c9898b073e399ac5eb67cb728a8c9
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-02-24 09:23:27 +0100

    check the header when posting marker

--- server/api.js
+++ server/api.js
@@ -62,6 +62,39 @@ const getRouter = (db) => {
   router.post('/markers', function (req, res) {
     const { latitude, longitude } = req.body;
     if (!latitude || !longitude) {
+      return res.status(400).json({ error: 'Lat or Long not set' });
+    }
+
+    const latitudeNum = Number(latitude);
+    const longitudeNum = Number(longitude);
+
+    if (isNaN(latitudeNum) || isNaN(longitudeNum)) {
+      return res.status(400).json({ error: 'Lat or Long has invalid form' });
+    }
+
+    const userId = req.headers['x-dymek-user-id']
+
+    if (!userId) {
+      return res.status(401).json({ error: 'You can\t post markers as not identified user' });
+    }
+
+    const id = uuid()
+    const params = {
+      TableName: MARKERS_TABLE,
+      Item: {id, latitude: latitudeNum, longitude: longitudeNum, userId, createdAt: new Date()},
+    };
+
+    db.put(params, (error, item) => {
+      if (error) {
+        res.status(400).json({ error: 'Could not create marker' });
+      }
+      res.json({ id, latitude, longitude, userId, createdAt: new Date() });
+    });
+  })
+
+  router.get('/users', function (req, res) {
+    const { latitude, longitude } = req.body;
+    if (!latitude || !longitude) {
       res.status(400).json({ error: 'Lat or Long not set' });
     }
 
