69b3ef2da4cf7e8a4ee893172a1a304b6e4098d8
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-03-05 08:19:05 +0100

    remove unnecessary imports

--- server/index.js
+++ server/index.js
@@ -6,13 +6,7 @@ const AWS = require('aws-sdk');
 const uuid = require('uuid/v4')
 const apiRouter = require('./api');
 
-const MARKERS_TABLE = process.env.MARKERS_TABLE;
-const IS_OFFLINE = process.env.IS_OFFLINE;
-const DEV_DB_PORT = process.env.DEV_DB_PORT
-
-
 app.use(bodyParser.json({ strict: false }));
 app.use('/api', apiRouter)
-
 
 module.exports.handler = serverless(app);
